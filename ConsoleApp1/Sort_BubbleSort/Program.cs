b2c1cc26360e6ce93389aaf81b8c46c1c6478240
pakornsrs <pakorn.srs@gmail.com>
2022-11-13 15:32:30 +0700

    First commit update bubble sort

---
+++ ConsoleApp1/Sort_BubbleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Sort_BubbleSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, -2, 12, 9, 20, 17, 2, -8, 11, 7 };
+
+            // display result
+            string arrString = "";
+            var sorted = BubbleSortForNum(nums);
+            foreach (var item in sorted)
+            {
+                arrString = arrString + item.ToString() + ", ";
+            }
+
+            Console.WriteLine(arrString);
+        }
+
+        private static int[] BubbleSortForNum(int[] nums)
+        {
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                bool Swapped = false;
+                for(int j = 0; j < nums.Length - 1; j++)
+                {
+                    if(nums[j] > nums[j + 1])
+                    {
+                        // Swap
+                        var temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+
+                        Swapped = true;
+                    }
+                }
+
+                if(Swapped == false)
+                {
+                    break;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
