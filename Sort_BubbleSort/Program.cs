2dcd6487661c3bb1eda2e2861572bf2e1c6c549f
pakornsrs <pakorn.srs@gmail.com>
2022-11-13 15:47:46 +0700

    Update SelectionSort

--- Sort_BubbleSort/Program.cs
+++ Sort_BubbleSort/Program.cs
@@ -19,7 +19,7 @@
 
         private static int[] BubbleSortForNum(int[] nums)
         {
-            for(int i = 0; i < nums.Length - 1; i++)
+            for(int i = 0; i < nums.Length; i++)
             {
                 bool Swapped = false;
                 for(int j = 0; j < nums.Length - 1; j++)
---
+++ Sort_SelectionSort/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Sort_SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, -2, 12, 9, 20, 17, 2, -8, 11, 7 };
+
+            // display result
+            string arrString = "";
+            var sorted = SelectionSortForNum(nums);
+            foreach (var item in sorted)
+            {
+                arrString = arrString + item.ToString() + ", ";
+            }
+
+            Console.WriteLine(arrString);
+        }
+
+        private static int[] SelectionSortForNum(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var minIndex = i;
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[j] < nums[minIndex])
+                    {
+                        // Record new min index
+                        minIndex = j;
+                    }
+
+                    if(j == nums.Length - 1 && minIndex != i)
+                    {
+                        // Swap
+                        var temp = nums[i];
+                        nums[i] = nums[minIndex];
+                        nums[minIndex] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
